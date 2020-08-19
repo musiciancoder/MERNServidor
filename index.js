3568dc922eb62a132f852ea180bea4731e9abd03
musiciancoder <andresguitarra1980@gmail.com>
2020-08-19 15:40:47 -0400

    232. Configurando Rutas de usuarios

--- index.js
+++ index.js
@@ -12,10 +12,13 @@ conectarDB();
 //puertos de la app; process.env.PORT es de Heroku
 const PORT = process.env.PORT || 4000;
 
-//Definir la pagina principal
+//importar rutas. Esto relaciona las URL con respectivos modulos
+app.use('/api/usuarios', require('./routes/usuarios')); //api se usa como ruta general
+
+/*//Definir la pagina principal (prueba al principio)
 app.get('/',(req,res)=>{
    // res.send('Hola Mundo'); //Para probar q funciona el servidor
-});
+});*/
 
 //arrancar la app
 app.listen(PORT, ()=>{
---
+++ routes/usuarios.js
@@ -0,0 +1,13 @@
+//Rutas para crear usuarios
+
+const express = require('express');
+const router = express.Router();
+
+//Crea un usuario
+//hacia URL api/usuarios
+
+router.post('/', ()=>{
+
+});
+
+module.exports = router; //para q este disponible en index.cl
