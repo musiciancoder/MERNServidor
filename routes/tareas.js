9b84fc4f3ad8576cd1dbcce3e6de6d24231c3a22
musiciancoder <andresguitarra1980@gmail.com>
2020-08-24 17:52:22 -0400

    255. Insertando tareas y el Proyecto al que pertenecen

--- routes/tareas.js
+++ routes/tareas.js
@@ -2,17 +2,17 @@ const express = require('express');
 const router = express.Router();
 const tareaController = require('../controllers/tareaController');
 const auth = require('../middleware/auth');
-const {check} = require('express-validator');
+const { check } = require('express-validator');
 
-//crear una tarea
-//api/tareas
+// crear una tarea
+// api/tareas
 router.post('/',
     auth,
     [
-        check('nombre', 'El nombre de la tarea es obligatorio').not().isEmpty()
-
+        check('nombre', 'El Nombre es obligatorio').not().isEmpty(),
+        check('proyecto', 'El Proyecto es obligatorio').not().isEmpty()
     ],
     tareaController.crearTarea
-    );
+);
 
 module.exports = router;
